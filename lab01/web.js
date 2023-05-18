55be43d71eb7771c19d228428835d67273eda7e9
hoangthanh2407 <thanhhcgch210988@fpt.edu.vn>
2023-05-18 10:49:52 +0700

    Hello world

---
+++ lab01/web.js
@@ -0,0 +1,14 @@
+// tạo web server với NodeJS */
+
+//1. khai báo & import thư viện http
+const http = require('http');
+//2. Khai báo host (server)
+const host = 'localhost';
+//3. Khai báo server pỏt (default : 3000)
+const port = 3000;
+//4. tạo server
+const server = http.createServer((request,respond)=>{
+    respond.write("<h1>This is NodeJS web server</h1>")
+})
+//5. Chạy server bằng cách Listen port
+server.listen(port);
